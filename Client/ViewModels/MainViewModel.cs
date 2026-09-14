f394262ce106d735e60380789a665170dfe2de68
JonyBonyy <balloonboyloveballoons@gmail.com>
2026-09-15 01:25:55 +0300

    Added LoginView files

--- Client/ViewModels/MainViewModel.cs
+++ Client/ViewModels/MainViewModel.cs
@@ -4,6 +4,5 @@ namespace Client.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    [ObservableProperty]
-    public partial string Greeting { get; set; } = "Welcome to Avalonia!";
+    [ObservableProperty] public partial string Greeting { get; set; } = "Welcome to Avalonia!";
 }
